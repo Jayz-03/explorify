20fa90c39b8ac704afd7ae352d94605e1d416fb0
Jayz <140088934+Jayz-03@users.noreply.github.com>
2024-07-18 11:39:16 +0800

    first commit

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iconsax/iconsax.dart';
 
+// ignore: must_be_immutable
 class LoginPage extends StatelessWidget {
   var height, width;
 
